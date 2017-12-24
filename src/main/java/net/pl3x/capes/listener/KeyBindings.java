2d9c321d74de9a63dd1d7c4eab59995b6d4df9e9
BillyGalbreath <Blake.Galbreath@GMail.com>
2017-12-24 14:00:25 -0600

    Add missing lang entries for key binding options

--- src/main/java/net/pl3x/capes/listener/KeyBindings.java
+++ src/main/java/net/pl3x/capes/listener/KeyBindings.java
@@ -5,11 +5,11 @@ import net.minecraftforge.fml.client.registry.ClientRegistry;
 import org.lwjgl.input.Keyboard;
 
 public class KeyBindings {
-    private final static String CATEGORY = "key.categories.pl3x";
+    private final static String CATEGORY = "key.categories.capes";
 
     public static KeyBinding capesGUI;
 
     public static void init() {
-        ClientRegistry.registerKeyBinding(capesGUI = new KeyBinding("key.capesGUI", Keyboard.KEY_B, CATEGORY));
+        ClientRegistry.registerKeyBinding(capesGUI = new KeyBinding("key.capes.gui", Keyboard.KEY_B, CATEGORY));
     }
 }
